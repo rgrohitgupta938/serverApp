c5578bcacffd324e5165ab91735dbeb24871559e
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2023-07-25 17:44:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+let express = require("express");
+let { carMaster, cars } = require("./carData");
+let app = express();
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD"
+  );
+  next();
+});
 var port = process.env.PORT || 2410;
+app.listen(port, () => console.log(`Node app listening on port ${port}`));
