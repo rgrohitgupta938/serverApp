6521af39be963108ba7771f9138fa9fa7d0585c2
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2023-07-29 12:41:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,75 +15,107 @@ app.use(function (req, res, next) {
 });
 var port = process.env.PORT || 2410;
 app.listen(port, () => console.log(`Node app listening on port ${port}`));
-let { studentsData } = require("./studentData");
-app.get("/svr/test", function (req, res) {
-  res.send("test Response 567889");
+
+let { studentsDat } = require("./studentsData");
+let fs = require("fs");
+let fname = "students.json";
+
+app.get("/svr/resetData", function (req, res) {
+  console.log(studentsDat);
+  let data = JSON.stringify(studentsDat);
+  fs.writeFile(fname, data, function (err) {
+    if (err) res.status(404).send(err);
+    else res.send("Data in file is reset");
+  });
 });
-app.get("/svr/students", function (req, res) {
-  let courseStr = req.query.course;
-  let grade = req.query.grade;
-  let sort = req.query.sort;
-  let arr1 = studentsData;
-  if (courseStr) {
-    let courseArr = courseStr.split(",");
-    arr1 = arr1.filter((st) => courseArr.find((cr) => cr === st.course));
-  }
-  if (grade) {
-    arr1 = arr1.filter((st) => st.grade === grade);
-  }
-  if (sort === "name") {
-    arr1.sort((p1, p2) => p1.name.localeCompare(p2.name));
-  }
-  if (sort === "course") {
-    arr1.sort((p1, p2) => p1.course.localeCompare(p2.course));
-  }
-  res.send(arr1);
+app.get("/svr/students", function (re, res) {
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let studentsArray = JSON.parse(data);
+      res.send(studentsArray);
+    }
+  });
 });
 app.get("/svr/students/:id", function (req, res) {
-  let id = req.params.id;
-  let students = studentsData.find((st) => st.id === +id);
-  if (students) {
-    res.send(students);
-  } else {
-    res.status(404).send("No student found");
-  }
+  let id = +req.params.id;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let studentsArray = JSON.parse(data);
+      let student = studentsArray.find((st) => st.id === id);
+      if (student) res.send(student);
+      else res.status(404).send("No student Found");
+    }
+  });
 });
 app.get("/svr/students/course/:name", function (req, res) {
   let name = req.params.name;
-  let students = studentsData.filter((st) => st.course === name);
-  res.send(students);
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let studentsArray = JSON.parse(data);
+      let arr1 = studentsArray.filter((st) => st.course === name);
+      res.send(arr1);
+    }
+  });
 });
 app.post("/svr/students", function (req, res) {
   let body = req.body;
-  console.log(body);
-  let maxid = studentsData.reduce(
-    (acc, curr) => (curr.id >= acc ? curr.id : acc),
-    0
-  );
-  let newid = maxid + 1;
-  let newStudent = { id: newid, ...body };
-  studentsData.push(newStudent);
-  res.send(newStudent);
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let studentsArray = JSON.parse(data);
+      let maxid = studentsArray.reduce(
+        (acc, curr) => (curr.id > acc ? curr.id : acc),
+        0
+      );
+      let newid = maxid + 1;
+      let newStudent = { ...body, id: newid };
+      studentsArray.push(newStudent);
+      let data1 = JSON.stringify(studentsArray);
+      fs.writeFile(fname, data1, function (err) {
+        if (err) res.status(404).send(err);
+        else res.send(newStudent);
+      });
+    }
+  });
 });
 app.put("/svr/students/:id", function (req, res) {
-  let id = +req.params.id;
   let body = req.body;
-  let inx = studentsData.findIndex((st) => st.id === id);
-  if (inx >= 0) {
-    let updateStu = { id: id, ...body };
-    studentsData[inx] = updateStu;
-    res.send(updateStu);
-  }
-  else{
-    res.status(404).send("No student Found");
-  }
+  let id = +req.params.id;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let studentsArray = JSON.parse(data);
+      let inx = studentsArray.findIndex((st) => st.id === id);
+      if (inx >= 0) {
+        let updatedStudent = { ...studentsArray[inx], ...body };
+        studentsArray[inx] = updatedStudent;
+        let data1 = JSON.stringify(studentsArray);
+        fs.writeFile(fname, data1, function (err) {
+          if (err) res.status(404).send(err);
+          else res.send(updatedStudent);
+        });
+      } else res.status(404).send("No student Found");
+    }
+  });
 });
-app.delete("/svr/students/:id",function(req,res){
-  let id = req.params.id;
-  let inx = studentsData.findIndex((st) => st.id === +id);
-  if(inx>=0){
-    let deletedStudent = studentsData.splice(inx,1);
-    res.send(deletedStudent);
-  }else res.status(404).send("No Student Found");
-  
-})
+app.delete("/svr/students/:id", function (req, res) {
+    let id = +req.params.id;
+    fs.readFile(fname, "utf-8", function (err, data) {
+      if (err) res.status(404).send(err);
+      else {
+        let studentsArray = JSON.parse(data);
+        let inx = studentsArray.findIndex((st) => st.id === id);
+        if (inx >= 0) {
+          let updatedStudent = studentsArray.splice(inx,1);
+          let data1 = JSON.stringify(studentsArray);
+          fs.writeFile(fname, data1, function (err) {
+            if (err) res.status(404).send(err);
+            else res.send(updatedStudent);
+          });
+        } else res.status(404).send("No student Found");
+      }
+    });
+  });
